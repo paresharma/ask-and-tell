f9e941254a877dfb888029d10594f76fe5f99d70
paresharma <paresharma@gmail.com>
2018-02-17 14:54:11 +0900

    Allow Google oauth credentials as ENV

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,8 +7,8 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :google_oauth2,
-    Rails.application.credentials.google[:client_id],
-    Rails.application.credentials.google[:client_secret],
+    Rails.application.credentials.dig(:google, :client_id) || ENV['GOOGLE_CLIENT_ID'],
+    Rails.application.credentials.dig(:google, :client_secret) || ENV['GOOGLE_CLIENT_SECRET'],
     { client_options: { ssl: { ca_file: Rails.root.join('cacert.pem').to_s }}}
   )
 end
